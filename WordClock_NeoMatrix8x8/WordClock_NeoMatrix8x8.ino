1337008eeb9930d50abc30ed342e1d60d1e92690
kghr labo <36298804+kghrlabo@users.noreply.github.com>
2020-05-31 23:25:14 +0900

    support ESP32
    Supports ESP32 and gets time from NTP.
    Adafruit's library is not stable on ESP32, so I use FastLED's library.

--- WordClock_NeoMatrix8x8/WordClock_NeoMatrix8x8.ino
+++ WordClock_NeoMatrix8x8/WordClock_NeoMatrix8x8.ino
@@ -1,58 +1,44 @@
 /*
-   WORD CLOCK - 8x8 NeoPixel Desktop Edition
-   by Andy Doro
+   WORD CLOCK - 8x8 NeoPixel Desktop Edition for ESP32
+   by kghr labo
+   (original for Trinket Pro by Andy Doro)
 
    A word clock using NeoPixel RGB LEDs for a color shift effect.
 
    Hardware:
-   - Trinket Pro 5V (should work with other Arduino-compatibles with minor modifications)
-   - DS1307 RTC breakout
-   - NeoPixel NeoMatrix 8x8
+      
+   - ESP32 Dev kit
+   - NeoPixel 64 LEDs (WS2812B)
 
 
    Software:
 
    This code requires the following libraries:
-
-   - RTClib https://github.com/adafruit/RTClib
-   - DST_RTC https://github.com/andydoro/DST_RTC
-   - Adafruit_GFX https://github.com/adafruit/Adafruit-GFX-Library
-   - Adafruit_NeoPixel https://github.com/adafruit/Adafruit_NeoPixel
-   - Adafruit_NeoMatrix https://github.com/adafruit/Adafruit_NeoMatrix
+   
+   - FastLED https://github.com/FastLED/FastLED
 
 
    Wiring:
-   - Solder DS1307 breakout to Trinket Pro, A2 to GND, A3 to PWR, A4 to SDA, A5 to SCL
-   If you leave off / clip the unused SQW pin on the RTC breakout, the breakout can sit right on top of the Trinket Pro
-   for a compact design! It'll be difficult to reach the Trinket Pro reset button, but you can activate the bootloader by
-   plugging in the USB.
-   - Solder NeoMatrix 5V to Trinket 5V, GND to GND, DIN to Trinket Pro pin 8.
-
+   - Solder NeoPixel 5V to ESP32 Dev kit VCC(5V), GND to GND, DIN to GOIO pin 16.
+   
 
    grid pattern
 
     A T W E N T Y D
     Q U A R T E R Y
     F I V E H A L F
-    D P A S T O R O
+    D P A S T O W I
     F I V E I G H T
     S I X T H R E E
     T W E L E V E N
     F O U R N I N E
 
 
-    Acknowledgements:
-    - Thanks Dano for faceplate / 3D models & project inspiration!
-
 */
 
 // include the library code:
-#include <Wire.h>
-#include <RTClib.h>
-#include <DST_RTC.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_NeoMatrix.h>
-#include <Adafruit_NeoPixel.h>
+#include <WiFi.h>
+#include <FastLED.h>
 
 // define how to write each of the words
 
@@ -79,13 +65,23 @@ uint64_t mask;
 #define TEN      mask |= 0x1010100
 #define ELEVEN   mask |= 0x3F00
 #define TWELVE   mask |= 0xF600
-#define ANDYDORO mask |= 0x8901008700000000
+#define ANDYDORO mask |= 0x8901008400040040
+
+#define WIFI     mask = 0x3C0000000
+#define WIFIER   mask = 0x3C0002010
+#define NTPER    mask = 0xC00004000002010
 
 // define pins
-#define NEOPIN 8  // connect to DIN on NeoMatrix 8x8
-#define RTCGND A2 // use this as DS1307 breakout ground 
-#define RTCPWR A3 // use this as DS1307 breakout power
+#define NEOPIN       16  // connect to DIN on NeoPixel
+#define NUM_LEDS     64  // 8x8
+#define LED_TYPE     WS2812B
+#define COLOR_ORDER  GRB
 
+CRGB leds[NUM_LEDS];
+
+// wifi setting
+const char* ssid     = "set your wifi ssid";
+const char* password = "set your wifi password";
 
 // brightness based on time of day- could try warmer colors at night?
 #define DAYBRIGHTNESS 40
@@ -95,40 +91,18 @@ uint64_t mask;
 #define MORNINGCUTOFF 7  // when does daybrightness begin?   7am
 #define NIGHTCUTOFF   22 // when does nightbrightness begin? 10pm
 
-
 // define delays
 #define FLASHDELAY 250  // delay for startup "flashWords" sequence
 #define SHIFTDELAY 100   // controls color shifting speed
 
-
-RTC_DS1307 RTC; // Establish clock object
-DST_RTC dst_rtc; // DST object
-
-DateTime theTime; // Holds current clock time
+struct tm theTime; // Holds current clock time
 
 int j;   // an integer for the color shifting effect
 
-// Do you live in a country or territory that observes Daylight Saving Time?
-// https://en.wikipedia.org/wiki/Daylight_saving_time_by_country
-// Use 1 if you observe DST, 0 if you don't. This is programmed for DST in the US / Canada. If your territory's DST operates differently,
-// you'll need to modify the code in the calcTheTime() function to make this work properly.
-//#define OBSERVE_DST 1
-
-
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//Adafruit_NeoPixel matrix = Adafruit_NeoPixel(64, NEOPIN, NEO_GRB + NEO_KHZ800);
-
-// configure for 8x8 neopixel matrix
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, NEOPIN,
-                            NEO_MATRIX_TOP  + NEO_MATRIX_LEFT +
-                            NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
-                            NEO_GRB         + NEO_KHZ800);
+// ntp setting
+const char* ntpServer = "time.google.com";
+const long  gmtOffset_sec = 3600*9; // JST +9
+const int   daylightOffset_sec = 3600*0; // no Daylight Saving
 
 
 void setup() {
@@ -137,39 +111,38 @@ void setup() {
   //Serial for debugging
   //Serial.begin(9600);
 
-  // set pinmodes
-  pinMode(NEOPIN, OUTPUT);
+  // initialize NeoPixel 
+  delay( 3000 ); // power-up safety delay
+  FastLED.addLeds<LED_TYPE, NEOPIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(DAYBRIGHTNESS);
+  FastLED.clear(); // Initialize all pixels to 'off'
+  FastLED.show();
 
-  // set analog pins to power DS1307 breakout!
-  pinMode(RTCGND, OUTPUT); // analog 2
-  pinMode(RTCPWR, OUTPUT); // analog 3
-
-  // set them going!
-  digitalWrite(RTCGND, LOW);  // GND for RTC
-  digitalWrite(RTCPWR, HIGH); // PWR for RTC
-
-  // start clock
-  Wire.begin();  // Begin I2C
-  RTC.begin();   // begin clock
-
-  if (! RTC.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    RTC.adjust(DateTime(__DATE__, __TIME__));
-    // DST? If we're in it, let's subtract an hour from the RTC time to keep our DST calculation correct. This gives us
-    // Standard Time which our DST check will add an hour back to if we're in DST.
-    DateTime standardTime = RTC.now();
-    if (dst_rtc.checkDST(standardTime) == true) { // check whether we're in DST right now. If we are, subtract an hour.
-      standardTime = standardTime.unixtime() - 3600;
+  // wifi connect
+  int retry = 0;
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500); // 500ms
+    retry ++;
+    mask = 0;
+    if (retry%2) WIFI;
+    applyMask();
+    
+    if (retry > 20) {
+      WIFIER;  // wifi connect timeout (500ms x 20 = 10 sec)
+      applyMask();
+      delay(30000); // hold 30 sec
+      ESP.restart(); // restart ESP32
     }
-    RTC.adjust(standardTime);
   }
+  Serial.println();
+  Serial.printf("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 
-
-  matrix.begin();
-  matrix.setBrightness(DAYBRIGHTNESS);
-  matrix.fillScreen(0); // Initialize all pixels to 'off'
-  matrix.show();
+  // Config NTP 
+  // If you need a DST, please extend it here.
+  configTime( gmtOffset_sec, daylightOffset_sec, ntpServer);
 
   // startup sequence... do colorwipe?
   // delay(500);
@@ -182,17 +155,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // get the time
-  theTime = dst_rtc.calculateTime(RTC.now()); // takes into account DST
-  // add 2.5 minutes to get better estimates
-  theTime = theTime.unixtime() + 150;
-
   adjustBrightness();
   displayTime();
 
-  //mode_moon(); // uncomment to show moon mode instead!
-
-
 }
-
-
