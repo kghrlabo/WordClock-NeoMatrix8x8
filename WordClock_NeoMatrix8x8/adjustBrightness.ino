b6ccb1daa69583be4ad4bc28a3b6e5f4bd85d3c2
kghr labo <36298804+kghrlabo@users.noreply.github.com>
2020-05-31 23:29:00 +0900

    support ESP32
    Use FastLED instead of Adafruit.

--- WordClock_NeoMatrix8x8/adjustBrightness.ino
+++ WordClock_NeoMatrix8x8/adjustBrightness.ino
@@ -2,14 +2,10 @@
 // change brightness based on the time of day.
 
 void adjustBrightness() {
-
-  // get time from the RTC
-  //DateTime thetime = RTC.now();
-  
   //change brightness if it's night time
   if (theTime.hour() < MORNINGCUTOFF || theTime.hour() > NIGHTCUTOFF) {
-    matrix.setBrightness(NIGHTBRIGHTNESS);
+    FastLED.setBrightness(NIGHTBRIGHTNESS);
   } else {
-    matrix.setBrightness(DAYBRIGHTNESS);
+    FastLED.setBrightness(DAYBRIGHTNESS);
   }
 }
